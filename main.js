d757a78645f18e616bc527d9ef6e534e42ad6cc4
Tanner <fossta@oregonstate.edu>
2021-06-05 19:49:54 -0700

    updated files

--- main.js
+++ main.js
@@ -18,9 +18,7 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
-app.use('/people_certs', require('./people_certs.js'));
 app.use('/employee', require('./employee.js'));
-app.use('/planets', require('./planets.js'));
 app.use('/', express.static('public'));
 
 app.use(function (req, res) {
